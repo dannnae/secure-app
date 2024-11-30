f004fd610ef0287fca40ea5c2a502d36622b6a92
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-11-30 19:38:14 -0300

    hosts

--- secure_blog/settings.py
+++ secure_blog/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['secure-app-fb64.onrender.com']
-
+ALLOWED_HOSTS = ['.onrender.com']
 
 # Application definition
 
