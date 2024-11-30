5cc82202ff7f53503b699c0dc884110e491c2e19
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-11-30 12:32:55 -0300

    cambios varios

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn secure_blog.wsgi
+web: gunicorn secure_blog.wsgi:applicatio
--- secure_blog/wsgi.py
+++ secure_blog/wsgi.py
@@ -1,14 +1,6 @@
-"""
-WSGI config for secure_blog project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
+# secure_blog/wsgi.py
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secure_blog.settings')
